1ca9846e1e801922c6400c4c8de699c76e8305af
GoldDever <69146353+GoldDever@users.noreply.github.com>
2020-11-23 15:02:44 +0600

    Дополнения

--- src/main/java/com/altyn/bootstrap/controller/MyController.java
+++ src/main/java/com/altyn/bootstrap/controller/MyController.java
@@ -41,29 +41,29 @@ public class MyController {
     @PostMapping()
     public String saveNewUser(@ModelAttribute("user") User user) {
         userService.save(user);
-        return "redirect:/allUsers";
+        return "redirect:/edit/admin";
     }
 
-    @GetMapping("/{id}/admin")
-    public String editUser(@PathVariable("id") long id, Model model) {
+    @GetMapping("/edit/admin")
+    public String editUser(/*@PathVariable("id") long id,*/ Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) auth.getPrincipal();
         model.addAttribute("userGotIn", user);
         model.addAttribute("users", userService.findAll());
-        model.addAttribute("user", userService.findById(id));
-        return "tren";
+       // model.addAttribute("user", userService.findById(id));
+        return "admin";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("user") User user, @PathVariable("id") long id) {
         userService.update(id, user);
-        return "redirect:/allUsers";
+        return "redirect:/edit/admin";
     }
 
     @DeleteMapping("/{id}/admin")
     public String delete(@PathVariable("id") long id) {
         userService.deleteById(id);
-        return "redirect:/{id}/admin";
+        return "redirect:/edit/admin";
     }
 
     @GetMapping("page/user")
